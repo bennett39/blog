dc5d0e3b4ee5dbd9744966ea7b156ca57baa51af
Bennett Garner <34491412+bennett39@users.noreply.github.com>
2021-04-25 13:51:37 -0400

    fix: tweak a few things to learn gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,16 +9,12 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
 
-  if (posts.length === 0) {
+  if (posts == undefined || posts.length === 0) {
     return (
       <Layout location={location} title={siteTitle}>
         <Seo title="All posts" />
         <Bio />
-        <p>
-          No blog posts found. Add markdown posts to "content/blog" (or the
-          directory you specified for the "gatsby-source-filesystem" plugin in
-          gatsby-config.js).
-        </p>
+        <p>No blog posts found.</p>
       </Layout>
     )
   }
